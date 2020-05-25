9ca9b6ec17c2e319535adfb6aa93010dabbf33cf
shlokpatel291 <47800401+shlokpatel291@users.noreply.github.com>
2020-05-25 21:32:24 +0530

    Add files via upload

---
+++ turtle/turtle.py
@@ -0,0 +1,12 @@
+import turtle
+
+turtle.bgcolor("black")
+turtle.speed(0)
+
+for i in range(6):
+    for colors in ["red","magenta","blue","cyan","green","yellow","white"]:
+        turtle.color(colors)
+        turtle.circle(100)
+        turtle.left(10)
+
+turtle.hideturtle()
